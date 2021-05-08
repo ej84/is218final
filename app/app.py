96423b7fa1c87d048d50a3cc97e0081ecbe80dfd
ej84 <ej84@njit.edu>
2021-05-08 19:31:05 -0400

    in progress: fixed dockerfile and app.py

---
+++ app/app.py
@@ -0,0 +1,139 @@
+from typing import List, Dict
+import simplejson as json
+from flask import Flask, request, Response, redirect
+from flask import render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
+
+app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'mlbPlayers'
+mysql.init_app(app)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    user = {'username': 'Players Project'}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblPlayersImport')
+    result = cursor.fetchall()
+    return render_template('index.html', title='Home', user=user, players=result)
+
+
+@app.route('/view/<int:player_id>', methods=['GET'])
+def record_view(player_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblPlayersImport WHERE id=%s', player_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', player=result[0])
+
+
+@app.route('/edit/<int:player_id>', methods=['GET'])
+def form_edit_get(player_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblPlayersImport WHERE id=%s', player_id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', player=result[0])
+
+
+@app.route('/edit/<int:player_id>', methods=['POST'])
+def form_update_post(player_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('Name'), request.form.get('Team'), request.form.get('Position'),
+                 request.form.get('Height_inches'), request.form.get('Weight_lbs'),
+                 request.form.get('Age'), player_id)
+    sql_update_query = """UPDATE tblPlayersImport t SET t.Name = %s, t.Team = %s, t.Position = %s, t.Height_inches = %s, t.Weight_lbs = %s, t.Age = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/players/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New Player Form')
+
+
+@app.route('/players/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('Name'), request.form.get('Team'), request.form.get('Position'),
+                 request.form.get('Height_inches'), request.form.get('Weight_lbs'),
+                 request.form.get('Age'))
+    sql_insert_query = """INSERT INTO tblPlayersImport (Name, Team , Position , Height_inches ,Weight_lbs ,Age) VALUES (%s, %s,%s, %s,%s, %s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/delete/<int:player_id>', methods=['POST'])
+def form_delete_post(player_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM tblPlayersImport WHERE id = %s """
+    cursor.execute(sql_delete_query, player_id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/api/v1/players', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblPlayersImport')
+    result = cursor.fetchall()
+    json_result = json.dumps(result)
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/players/<int:player_id>', methods=['GET'])
+def api_retrieve(player_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblPlayersImport WHERE id=%s', player_id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result)
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/players/', methods=['POST'])
+def api_add() -> str:
+    content = request.json
+    cursor = mysql.get_db().cursor()
+    inputData = (content['Name'], content['Team'], content['Position'], content['Height_inches'], content['Weight_lbs'], content['Age'])
+    sql_insert_query = """INSERT INTO tblPlayersImport (Name, Team , Position , Height_inches ,Weight_lbs ,Age) VALUES (%s, %s,%s, %s,%s, %s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/players/<int:player_id>', methods=['PUT'])
+def api_edit(player_id) -> str:
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['Name'], content['Team'], content['Position'], content['Height_inches'], content['Weight_lbs'],
+                 content['Age'], player_id)
+    sql_update_query = """UPDATE tblPlayersImport t SET t.Name = %s, t.Team = %s, t.Position = %s, t.Height_inches = %s,
+    t.Weight_lbs = %s, t.Age = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/players/<int:player_id>', methods=['DELETE'])
+def api_delete(player_id) -> str:
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM tblPlayersImport WHERE id = %s """
+    cursor.execute(sql_delete_query, player_id)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
